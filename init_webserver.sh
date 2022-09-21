a3b8e2306fcafae03f93230887829fd8f6d72976
tychocity <tychocity@gmail.com>
2022-09-21 11:09:45 -0300

    limpieza comandos inicio

--- init_webserver.sh
+++ init_webserver.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Create mount volume for logs
-#  sudo su - root
-#  mkfs.ext4 /dev/sdf
-#  mount -t ext4 /dev/sdf /var/log
-
 # Install & Start nginx server
   yum search nginx 
   amazon-linux-extras install nginx1 -y
